56c80c93db5c9607e9fc4698f114ccf29d92d352
Rafael Freitas <rafael_cssa@hotmail.com>
2022-02-01 20:56:55 -0300

    BD com JDBC: Classe Department

---
+++ src/application/Program07.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program07 {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
